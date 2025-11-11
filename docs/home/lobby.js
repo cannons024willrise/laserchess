628002efd9ebca4c5e7ff31b75b0b2da4a2fd745
cannons024willrise <later5143@gmail.com>
2025-11-11 19:57:48 +0530

    Update lobby.js
    Signed-off-by: cannons024willrise <later5143@gmail.com>

--- docs/home/lobby.js
+++ docs/home/lobby.js
@@ -1,14 +1,4 @@
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyCVHWRoVyuDnKRPY4EQbmyeY_rRR56XpEg",
-    authDomain: "laserchessnexus-lobby-db-auth.firebaseapp.com",
-    databaseURL: "https://laserchessnexus-lobby-db-auth-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "laserchessnexus-lobby-db-auth",
-    storageBucket: "laserchessnexus-lobby-db-auth.firebasestorage.app",
-    messagingSenderId: "1054469827964",
-    appId: "1:1054469827964:web:0fe70a1fddb5b5d27588f9"
-  };
-
   firebase.initializeApp(firebaseConfig);
   const db = firebase.database();
 
@@ -28,6 +18,7 @@
       console.log('Lobby is empty');
     }
   });
+
 
 
 
