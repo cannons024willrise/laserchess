0f4fe1e4252ef1dce9261faa85efe63f0161e3d4
cannons024willrise <later5143@gmail.com>
2025-11-11 18:42:37 +0530

    Update lobby.js
    Create a simple real time sync read test from the lobby RTDB.
    
    Signed-off-by: cannons024willrise <later5143@gmail.com>

--- docs/home/lobby.js
+++ docs/home/lobby.js
@@ -1,12 +1,24 @@
-<script>
-  // Get a reference to the database (using compat version loaded by your CDN scripts)
+// Initialize Firebase
+  const firebaseConfig = {
+    apiKey: "AIzaSyCVHWRoVyuDnKRPY4EQbmyeY_rRR56XpEg",
+    authDomain: "laserchessnexus-lobby-db-auth.firebaseapp.com",
+    databaseURL: "https://laserchessnexus-lobby-db-auth-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "laserchessnexus-lobby-db-auth",
+    storageBucket: "laserchessnexus-lobby-db-auth.firebasestorage.app",
+    messagingSenderId: "1054469827964",
+    appId: "1:1054469827964:web:0fe70a1fddb5b5d27588f9"
+  };
+
+  // Initialize Firebase App
+  firebase.initializeApp(firebaseConfig);
+
+  // Reference to your Realtime Database
   const db = firebase.database();
 
-  // Reference to your lobby rooms
+  // Reference to lobby rooms
   const lobbyRef = db.ref('lobby/rooms');
 
   // Real-time listener
   lobbyRef.on('value', (snapshot) => {
     console.log('Current Lobby Rooms:', snapshot.val());
   });
-</script>
